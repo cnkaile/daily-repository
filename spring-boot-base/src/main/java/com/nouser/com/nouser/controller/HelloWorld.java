438a742195c314af0666de2b3a5d53221d807608
zhoukl <kaile95@163.com>
2020-11-30 01:54:25 +0800

    add log4j2 extend other logFormark

--- spring-boot-base/src/main/java/com/nouser/com/nouser/controller/HelloWorld.java
+++ spring-boot-base/src/main/java/com/nouser/com/nouser/controller/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.nouser.com.nouser.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/init")
 public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
     @GetMapping("/helloWorld")
     public String helloWorld(String name){
+        logger.debug("{}, this is debug log", name);
+        logger.info("{}, this is info log", name);
+        logger.warn("{}, this is warn log", name);
+        logger.error("{}, this is error log", name);
+
         return name + ", Hello MyWorld.\n";
+
     }
 }
